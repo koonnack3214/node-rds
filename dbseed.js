e58e0c98fb7b292b98f604976270c8d36919e49f
koonnack3214 <kajbandit78@gmail.com>
2024-02-11 16:30:22 +0700

    Comleted feature #1

---
+++ dbseed.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host: "database-236.c7om482wqwx6.ap-southeast-2.rds.amazonaws.com",
+    user: "admin",
+    password:"PgpYTfFPf7CiACBXnIPO"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+
+    con.query('CREATE DATABASE IF NOT EXISTS main;');
+    con.query('USE main;');
+    con.query('CREATE TABLE IF NOT EXISTS users(id int NOT NULL AUTO_INCREMENT, username varchar(30), '+
+    'email varchar(255), age int, PRIMARY KEY(id));', function(error, result, fields) {
+        console.log(result);
+    });
+    con.end();
+});
